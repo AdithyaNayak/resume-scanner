047a7df80b2aaef623591703453fd97c48ab4b32
Secretabhi07 <140100483+Secretabhi07@users.noreply.github.com>
2023-11-22 20:43:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,50 @@ def filter_resumes(resumes, filters):
         if key == 'experience':
             filtered_resumes = [resume for resume in filtered_resumes if getattr(resume, key) >= value]
         elif key == 'skills':
+            
             filtered_resumes = [resume for resume in filtered_resumes if value.lower() in [skill.lower() for skill in resume.skills]]
         else:
             filtered_resumes = [resume for resume in filtered_resumes if getattr(resume, key) == value]
     return filtered_resumes
+
+def read_applicant_resumes(file_path):
+    resumes = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for i in range(0, len(lines), 5):
+            name = lines[i].strip().split(': ')[1]
+            education = lines[i + 1].strip().split(': ')[1]
+            skills = lines[i + 2].strip().split(': ')[1]
+            experience = lines[i + 3].strip().split(': ')[1]
+            domain = lines[i + 4].strip().split(': ')[1]
+            resumes.append(ApplicantResume(name, education, skills, experience, domain))
+    return resumes
+
+def write_applicant_resume(name, education, skills, experience, domain):
+    file_name = f"resume_{name.lower().replace(' ', '_')}.txt"
+    with open(file_name, 'w') as file:
+        file.write(f"Name: {name}\n")
+        file.write(f"Education: {education}\n")
+        file.write(f"Skills: {skills}\n")
+        file.write(f"Experience: {experience}\n")
+        file.write(f"Domain: {domain}\n")
+    return file_name
+
+def applicant_resume_page():
+    st.title("Applicant Resume Page")
+    st.subheader("Enter Your Resume Details")
+
+    name = st.text_input("Full Name:")
+    education = st.text_input("Education:")
+    skills = st.text_input("Skills (comma-separated):")
+    experience = st.text_input("Experience (in years):")
+    domain = st.text_input("Domain:")
+
+    if st.button("Submit Resume"):
+        if name and education and skills and experience and domain:
+            write_applicant_resume(name, education, skills, experience, domain)
+            st.success("Resume submitted successfully!")
+        else:
+            st.warning("Please fill in all the fields.")
+
+
